5ed79b05edd544c603fff7449dcdeb3a737840fb
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-26 17:45:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,32 @@
 def consolidate_cart(cart)
-  # code here
+  cart_hash = {}
+  cart.each{
+    |array| array.each do
+    |item, values| cart_hash[item] ||= values
+    cart_hash[item][:count] ? cart_hash[item][:count] +=1 :
+    cart_hash[item][:count] = 1 
+  end
+  }
+return cart_hash
+
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each{
+    |coupon| coupon.each do
+    |attribute, value| 
+    name = coupon[:item]
+    
+    if cart[name] && cart[name][:count] >= coupon[:num]
+      if cart["#{name} W/COUPON"][:count] += 1 
+      else 
+        cart["#{name} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[:clearance], :count => 1 
+      end
+    else 
+    end
+  end
+  }
+  
 end
 
 def apply_clearance(cart)
