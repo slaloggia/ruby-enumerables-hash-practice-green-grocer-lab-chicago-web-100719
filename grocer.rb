5beae42d9367111720dc03a68d0bd7efb8e296ce
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-26 18:48:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -36,7 +36,7 @@ def apply_clearance(cart)
     |item| item.each do
       |name, attributes|
       if name[:clearance] == true 
-        name[:price] = (name[:price] * ).round
+        name[:price] = name[:price] * 0.80
       end
     end
   }
