eebb0c34125c49215195775fcc46039c217a027b
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-26 17:48:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -23,7 +23,7 @@ def apply_coupons(cart, coupons)
       else 
         cart["#{name} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[:clearance], :count => 1 
       end
-      
+      cart[name][:count] -= coupon[:num]
     else 
     end
   end
